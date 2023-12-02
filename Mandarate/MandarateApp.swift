82e50e2d87c2a3d5a7536725e51fdbe072e9eff1
milliwonkim <rldnjs9347@gmail.com>
2023-12-02 18:42:15 +0900

    Initial Commit

---
+++ Mandarate/MandarateApp.swift
@@ -0,0 +1,20 @@
+//
+//  MandarateApp.swift
+//  Mandarate
+//
+//  Created by 김기원 on 2023/12/02.
+//
+
+import SwiftUI
+
+@main
+struct MandarateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
